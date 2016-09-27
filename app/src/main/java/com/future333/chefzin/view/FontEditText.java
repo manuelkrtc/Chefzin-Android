86e0f65c8f7b90b7e7ba3af0c604fe52e052b4d5
manueldelytably <manueldelytably@gmail.com>
2016-09-27 11:01:15 -0500

    change visual, and add style letter

---
+++ app/src/main/java/com/future333/chefzin/view/FontEditText.java
@@ -0,0 +1,42 @@
+package com.future333.chefzin.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import clickdelivery.pidefarma.R;
+
+public class FontEditText extends EditText {
+
+	public FontEditText(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(attrs);
+	}
+
+	public FontEditText(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs);
+
+	}
+
+	public FontEditText(Context context) {
+		super(context);
+		init(null);
+	}
+	
+	private void init(AttributeSet attrs) {
+		if (attrs!=null) {
+
+			TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FontTextView);
+			 String fontName = a.getString(R.styleable.FontTextView_fontName);
+			 if (fontName!=null && !this.isInEditMode()) {
+				 Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/"+fontName);
+				 setTypeface(myTypeface);
+			 }
+			 a.recycle();
+		}
+	}
+	
+}
