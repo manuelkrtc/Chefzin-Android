215281bea2a07b058ac8f9476e69f1725deefa29
manueldelytably <manueldelytably@gmail.com>
2016-10-11 14:40:10 -0500

    cambion boton agregar

---
+++ app/src/main/java/com/future333/chefzin/model/Controller/Cart.java
@@ -0,0 +1,26 @@
+package com.future333.chefzin.model.Controller;
+
+import com.future333.chefzin.model.Horary;
+import com.future333.chefzin.model.Product;
+
+import java.util.ArrayList;
+
+/**
+ * Created by manuel on 11/10/16.
+ */
+public class Cart {
+
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+
+}
