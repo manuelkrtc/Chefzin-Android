67477e19a27f87f33ee9af17cf82a0728fffbd74
Manuel Paez <manuel@pidefarma.com>
2016-11-17 21:14:03 -0500

    add request permissions location (incomplete)

--- app/src/main/java/com/future333/chefzin/Fragment/FragmentMap.java
+++ app/src/main/java/com/future333/chefzin/Fragment/FragmentMap.java
@@ -8,12 +8,15 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.future333.chefzin.AppHandler;
 import com.future333.chefzin.R;
+import com.future333.chefzin.tools.ViewTools;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
@@ -29,6 +32,8 @@ public class FragmentMap extends Fragment {
 
     MapView mapView;
     GoogleMap map;
+
+   static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
     public static FragmentMap newInstance() {
         return new FragmentMap();
@@ -54,21 +59,72 @@ public class FragmentMap extends Fragment {
         mapView = (MapView) v.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
 
+
+
+
+
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
-                if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+
+
+                if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(ctx, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        ActivityCompat.requestPermissions(ctx, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+                    } else {
+
+                        ViewTools.msj(ctx,"La cago tiene q activar esta mierda manualkmenet");
+//                        ActivityCompat.requestPermissions(ctx,
+//                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                                MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+                    }
+                }else{
+                    map.setMyLocationEnabled(true);
                 }
-                map.setMyLocationEnabled(true);
+
+//                if (ContextCompat.checkSelfPermission(this,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                        != PackageManager.PERMISSION_GRANTED) {
+//
+//                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+//
+//                    } else {
+//
+//                        ActivityCompat.requestPermissions(this,
+//                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+//
+//                    }
+//                }
+
+
+
+
+
+
+
+
+
+
+
+
+//                if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    // TODO: Consider calling
+//                    //    ActivityCompat#requestPermissions
+//                    // here to request the missing permissions, and then overriding
+//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                    //                                          int[] grantResults)
+//                    // to handle the case where the user grants the permission. See the documentation
+//                    // for ActivityCompat#requestPermissions for more details.
+//                    return;
+//                }
+//                map.setMyLocationEnabled(true);
 
             }
         });
@@ -110,6 +166,23 @@ public class FragmentMap extends Fragment {
 
 
     @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ViewTools.msj(ctx,"Cago1");
+                } else {
+                    ViewTools.msj(ctx,"cago2");
+                }
+                return;
+            }
+
+        }
+    }
+
+        @Override
     public void onResume() {
         mapView.onResume();
         super.onResume();
