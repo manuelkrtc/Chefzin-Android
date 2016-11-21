d31c36a8b0ccacbe4728e53a9d63b6b8778d733b
manueldelytably <manueldelytably@gmail.com>
2016-11-21 09:35:42 -0500

    add request permissions location and check enable location

--- app/src/main/java/com/future333/chefzin/Fragment/FragmentMap.java
+++ app/src/main/java/com/future333/chefzin/Fragment/FragmentMap.java
@@ -60,171 +60,21 @@ public class FragmentMap extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_maps, container, false);
 
-
         // Gets the MapView from the XML layout and creates it
         mapView = (MapView) v.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
-
-
-
-
-
-
-
-
-
-
-
-
-//        LocationManager lm = (LocationManager)ctx.getSystemService(Context.LOCATION_SERVICE);
-//        boolean gps_enabled = false;
-//        boolean network_enabled = false;
-//
-//        try {
-//            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-//            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//        } catch(Exception ex) {}
-//
-//        if(!gps_enabled && !network_enabled) {
-//            // notify user
-//            AlertDialog.Builder dialog = new AlertDialog.Builder(ctx);
-//            dialog.setMessage("gps_network_not_enabled");
-//            dialog.setPositiveButton("open_location_settings", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-//                    // TODO Auto-generated method stub
-//                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-//                    ctx.startActivity(myIntent);
-//                    //get gps
-//                }
-//            });
-//            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//
-//                @Override
-//                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-//                    // TODO Auto-generated method stub
-//
-//                }
-//            });
-//            dialog.show();
-//        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
 
-
-                ToolsPermissions.checkPermissionsLocation(ctx);
-
-//                if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//
-//
-//                    if (ActivityCompat.shouldShowRequestPermissionRationale(ctx, Manifest.permission.ACCESS_FINE_LOCATION)) {
-//                        ActivityCompat.requestPermissions(ctx, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-//
-//                    } else {
-//
-//                        ToolsView.msj(ctx,"La cago tiene q activar esta mierda manualkmenet");
-////                        ActivityCompat.requestPermissions(ctx,
-////                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-////                                MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-//
-//                    }
-//                }else{
-//                    map.setMyLocationEnabled(true);
-//                }
-
-//                if (ContextCompat.checkSelfPermission(this,
-//                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                        != PackageManager.PERMISSION_GRANTED) {
-//
-//                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-//                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-//
-//                    } else {
-//
-//                        ActivityCompat.requestPermissions(this,
-//                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-//                                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-//
-//                    }
-//                }
-
-
-
-
-
-
-
-
-
-
-
-
-//                if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                    // TODO: Consider calling
-//                    //    ActivityCompat#requestPermissions
-//                    // here to request the missing permissions, and then overriding
-//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                    //                                          int[] grantResults)
-//                    // to handle the case where the user grants the permission. See the documentation
-//                    // for ActivityCompat#requestPermissions for more details.
-//                    return;
-//                }
-//                map.setMyLocationEnabled(true);
-
+                if(ToolsPermissions.enabledPermissionsLocation(ctx)){
+                    map.setMyLocationEnabled(true);
+                    ToolsPermissions.enableLocation(ctx);
+                }
             }
         });
-
-
-        // Gets to GoogleMap from the MapView and does initialization stuff
-//        mapView.getMapAsync(new OnMapReadyCallback() {
-//            @Override
-//            public void onMapReady(GoogleMap googleMap) {
-////                map = googleMap;
-//
-////                map.getUiSettings().setMyLocationButtonEnabled(true);
-////                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-////                    // TODO: Consider calling
-////                    //    ActivityCompat#requestPermissions
-////                    // here to request the missing permissions, and then overriding
-////                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-////                    //                                          int[] grantResults)
-////                    // to handle the case where the user grants the permission. See the documentation
-////                    // for ActivityCompat#requestPermissions for more details.
-////                    return;
-////                }
-////                map.setMyLocationEnabled(true);
-//
-//
-////                MapsInitializer.initialize(getActivity());
-//
-//                // Updates the location and zoom of the MapView
-////                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-////                map.animateCamera(cameraUpdate);
-//            }
-//        });
-
-
-
 
         return v;
     }
--- app/src/main/java/com/future333/chefzin/tools/ToolsPermissions.java
+++ app/src/main/java/com/future333/chefzin/tools/ToolsPermissions.java
@@ -2,8 +2,13 @@ package com.future333.chefzin.tools;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
@@ -16,17 +21,51 @@ public class ToolsPermissions {
     static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
 
-    public static boolean checkPermissionsLocation(Activity ctx){
+    public static boolean enabledPermissionsLocation(Activity ctx){
         if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(ctx, Manifest.permission.ACCESS_FINE_LOCATION)){
                 ActivityCompat.requestPermissions(ctx, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_FINE_LOCATION);
             } else {
-                ToolsView.msj(ctx,"La cago tiene q activar esta mierda manualmente");
+                ToolsView.msj(ctx,"Si desea activar los permisos de localización debes acceder a la configuración del dispositivo.");
             }
             return false;
         }
         return true;
     }
 
+    public static void enableLocation(final Activity ctx){
+        LocationManager lm = (LocationManager)ctx.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            // notify user
+            AlertDialog.Builder dialog = new AlertDialog.Builder(ctx);
+            dialog.setTitle("Chefzin requiere localización");
+            dialog.setMessage("El GPS no se encuentra activado.");
+            dialog.setPositiveButton("Ajustes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    ctx.startActivity(myIntent);
+                }
+            });
+            dialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+            dialog.show();
+        }
+    }
 }
